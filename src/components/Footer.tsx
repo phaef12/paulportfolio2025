dcaa69f2050cbe052e1bee63aa8a2be45a9a3075
Paul Haefeli <phaef12@gmail.com>
2025-09-05 17:00:08 -0500

    update footer with test date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-blue-200 text-center py-4 shadow-t">
-      © {new Date().getFullYear()} Paul Haefeli. All rights reserved. Hosted on Azure.
+      © {new Date().getFullYear()} Paul Haefeli. All rights reserved. Hosted on Azure. v09052025T1700
     </footer>
   );
 }
